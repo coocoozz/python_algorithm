c5e96f6a79d62e32ab6110e662dbbc3e2c1fd211
kwanghwan.kim <coocoozz@naver.com>
2019-05-06 18:40:29 +0900

    make insert_sort with ascending and descending

--- sorting/insert_sort.py
+++ sorting/insert_sort.py
@@ -7,7 +7,7 @@ class InsertSort:
         self.list[index1] = self.list[index2]
         self.list[index2] = val
 
-    def sort(self):
+    def ascending(self):
         for index in range(1, len(self.list)):
             cur_index = index
             while cur_index > 0:
@@ -16,6 +16,15 @@ class InsertSort:
                 else:
                     self.swap(cur_index, cur_index-1)
                     cur_index -= 1
+        return self.list
 
-    def print(self):
-        print(self.list)
+    def descending(self):
+        for index in range(1, len(self.list)):
+            cur_index = index
+            while cur_index > 0:
+                if self.list[cur_index] < self.list[cur_index-1]:
+                    break
+                else:
+                    self.swap(cur_index, cur_index-1)
+                    cur_index -= 1
+        return self.list
