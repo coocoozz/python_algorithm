138d87863ca78a5f79bcfcc5cd738544c49ba9dc
kwanghwan.kim <coocoozz@naver.com>
2019-05-06 18:24:10 +0900

    insert sorting

---
+++ sorting/insert_sort.py
@@ -0,0 +1,21 @@
+class InsertSort:
+    def __init__(self, list):
+        self.list = list
+
+    def swap(self, index1, index2):
+        val = self.list[index1]
+        self.list[index1] = self.list[index2]
+        self.list[index2] = val
+
+    def sort(self):
+        for index in range(1, len(self.list)):
+            cur_index = index
+            while cur_index > 0:
+                if self.list[cur_index] > self.list[cur_index-1]:
+                    break
+                else:
+                    self.swap(cur_index, cur_index-1)
+                    cur_index -= 1
+
+    def print(self):
+        print(self.list)
