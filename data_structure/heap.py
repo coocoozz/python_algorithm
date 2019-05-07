5b952b5acb192375f940bf9a1b6ae6769a5da980
coocoozz <coocoozz@naver.com>
2019-05-07 21:39:04 +0900

    Update heap.py
    bug fix for Heap data structure

--- data_structure/heap.py
+++ data_structure/heap.py
@@ -56,6 +56,31 @@ class Heap:
 
         return ret
 
+    def remove_max_heap(self):
+        ret = self.list[0]
+        self.list[0] = self.list[len(self.list) - 1]
+        del self.list[len(self.list) - 1]
+
+        cur_index = 0
+        while cur_index < len(self.list):
+            left_child_index = cur_index * 2 + 1
+            if left_child_index >= len(self.list):
+                break
+
+            next_index = left_child_index
+
+            right_child_index = cur_index * 2 + 2
+            if right_child_index < len(self.list) and self.list[right_child_index] > self.list[left_child_index]:
+                next_index = right_child_index
+
+            if self.list[cur_index] < self.list[next_index]:
+                self.swap(cur_index, next_index)
+                cur_index = next_index
+            else:
+                break
+
+        return ret
+
     def remove_min_heap(self):
         ret = self.list[0]
         self.list[0] = self.list[len(self.list) - 1]
@@ -63,15 +88,17 @@ class Heap:
 
         cur_index = 0
         while cur_index < len(self.list):
-            next_index = cur_index
-            for i in [1, 2]:
-                child_index = cur_index * 2 + i
-                if child_index < len(self.list) and self.list[next_index] > self.list[child_index]:
-                    next_index = child_index
-                else:
-                    break
+            left_child_index = cur_index * 2 + 1
+            if left_child_index >= len(self.list):
+                break
 
-            if cur_index != next_index:
+            next_index = left_child_index
+
+            right_child_index = cur_index * 2 + 2
+            if right_child_index < len(self.list) and self.list[right_child_index] < self.list[left_child_index]:
+                next_index = right_child_index
+
+            if self.list[cur_index] > self.list[next_index]:
                 self.swap(cur_index, next_index)
                 cur_index = next_index
             else:
